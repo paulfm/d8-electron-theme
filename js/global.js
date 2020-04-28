98d99fe6e45b7cb31bd0ca4d8c4589df8a3622dd
paulfm <sellpahomes@gmail.com>
2020-04-28 15:10:24 -0400

    Initial commit for working subtheme

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.electron = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
